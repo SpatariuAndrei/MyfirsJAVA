ce0df88ba8b71211a9d155fc16f5d7540b195494
utilisateur <1234>
2019-06-12 22:59:35 +0300

    05.06.2019 Animal Rescue

---
+++ lab2/Calculator/IfElseSwitch.java
@@ -0,0 +1,7 @@
+package lab2.Calculator;
+
+public class IfElseSwitch {
+
+
+
+}
---
+++ lab2/Calculator/LogicalOP.java
@@ -0,0 +1,18 @@
+package lab2.Calculator;
+
+public class LogicalOP {
+
+    public int getHighNumber (int a, int b){
+
+        if (a>b) {
+
+            return a;
+        }else {
+            return b ;
+        }
+
+    }
+
+
+
+}
