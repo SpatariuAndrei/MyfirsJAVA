4480a534b44ba9fe6b22d573b99384431377ce26
utilisateur <1234>
2019-07-19 21:49:33 +0300

    Tema program Java

--- lab7/Run.java
+++ lab7/Run.java
@@ -1,16 +1,28 @@
 package lab7;
 
+
+
 public class Run {
 
     private Read read = new Read();
     private Calculator calc = new Calculator();
+    private Main main = new Main();
+
+
+
+    private void mainMenu() {
+        System.out.println("Hello!\n" +
+                "Please select a value from the menu:\n" +
+                "1. Math operations \n" +
+                "2. Converters \n");
+    }
 
 
 
 
 
 
-    private void printOperations() {
+    private void printMathOperations() {
         System.out.println("Hello!\n" +
                 "Please select a value from the menu:\n" +
                 "1. Sum\n" +
@@ -19,9 +31,14 @@ public class Run {
                 "4. Division\n"   +
                 "5. Average\n"    +
                 "6. DivisionRemainder\n" +
-                "7. Celsius To Fahrenheit convertor \n"  +
-                "8. Inch to meters convertor \n" +
-                "9. Speed in km/hours convertor \n" +
+                "0.EXIT PROGRAM!"      );
+    }
+    private void printConverters() {
+        System.out.println("Hello!\n" +
+                "Please select a value from the menu:\n" +
+                "1. Celsius To Fahrenheit convertor \n"  +
+                "2. Inch to meters convertor \n" +
+                "3. Speed in km/hours convertor \n" +
                 "0. EXIT PROGRAM ");
 
 
@@ -29,61 +46,111 @@ public class Run {
 
 
 
-
-
-
-
-    private boolean selectItemFromMenu() {
+    private boolean selectItemFromMainMenu() {
         System.out.print("Please select menu number: ");
-        int numberFromUser = read.getInt();
-        switch (numberFromUser) {
+        int numberFromUserMainMenu = read.getInt();
+        switch (numberFromUserMainMenu) {
+
             case 1:
-                sum();
+                printMathOperations();
+                selectItemFromMathOperations();
                 return true;
             case 2:
-                difference();
+                printConverters();
+                selectItemFromConverters();
                 return true;
-            case 3:
-                multiplication();
-                return true;
-            case 4:
-                division();
-                return true;
-            case 5:
-                average();
-                return true;
-            case 6:
-                divisionRemainder();
-                return true;
-            case 7:
-                temperatureFromFhrToCelsius();
-                return true;
-            case 8:
-                distanceInchToMeters();
-                return true;
-            case 9:
-                speedKmPerHour();
-                return true;
-
-            case 0:
-                System.out.println("Bye!");
-                return false;
+            case  0:
+            System.out.println("Bye!");
+            return false;
             default:
 
                 System.out.println("Try again!");
                 return true;
 
         }
+        }
+        public void runProgramMain() {
+          do{
+              mainMenu();
 
+          } while (selectItemFromMainMenu());
+        }
+
+
+
+
+    private boolean selectItemFromMathOperations() {
+      System.out.print("Please select menu number: ");
+        int numberFromUser = read.getInt();
+       switch (numberFromUser) {
+          case 1:
+              sum();
+               return true;
+           case 2:
+              difference();
+               return true;
+          case 3:
+               multiplication();
+              return true;
+         case 4:
+              division();
+              return true;
+         case 5:
+               average();
+             return true;
+        case 6:
+               divisionRemainder();
+              return true;
+           case 0:
+                System.out.println("Bye!");
+              return false;
+          default:
+
+              System.out.println("Try again!");
+              return true;
+
+      }
+   }
+        public void runProgram() {
+            do {
+                printMathOperations();
+
+            } while (selectItemFromMathOperations());
+        }
+
+
+        private boolean selectItemFromConverters() {
+            System.out.print("Please select menu number: ");
+            int numberFromUserConverters = read.getInt();
+            switch (numberFromUserConverters) {
+        case 1:
+        temperatureFromFhrToCelsius();
+        return true;
+        case 2:
+        distanceInchToMeters();
+        return true;
+        case 3:
+        speedKmPerHour();
+        return true;
+
+        case 0:
+        System.out.println("Bye!");
+        return false;
+        default:
+
+        System.out.println("Try again!");
+        return true;
     }
 
+        }
 
 
-    public void runProgram() {
+    public void runProgram1() {
+
         do {
-            printOperations();
+            printConverters();
 
-        } while (selectItemFromMenu());
+        } while (selectItemFromConverters());
     }
 
 
